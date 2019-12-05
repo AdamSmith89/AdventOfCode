a75c97eda4f7df774981d11965e07857c1febda8
Adam Smith <atsmith@beyondtrust.com>
2019-12-05 14:50:26 +0000

    Refactored to use set_intersection - much much much much much much quicker!

--- 2019/src/day3.cpp
+++ 2019/src/day3.cpp
@@ -2,18 +2,12 @@
 
 #include <fstream>
 #include <vector>
-#include <deque>
 #include <string>
 #include <sstream>
-#include <thread>
-#include <iostream>
 #include <algorithm>
-
-using namespace std::chrono_literals;
 
 namespace day3
 {
-    auto const UI_DELAY = 0.1s;
     char const UP = 'U';
     char const DOWN = 'D';
     char const RIGHT = 'R';
@@ -28,7 +22,6 @@ namespace day3
     struct Path
     {
         std::vector<Step> steps;
-        char id;
     };
 
     struct Intersection
@@ -42,13 +35,12 @@ namespace day3
     {
         std::vector<Path> paths;
 
-        std::ifstream ifs("resources/day3.txt");
+        std::ifstream ifs("resources/day3_test3.txt");
         std::string path;
-        char id = '1';
+
         while (std::getline(ifs, path))
         {
             Path newPath;
-            newPath.id = id++;
 
             std::stringstream pathStream(path);
             std::string step;
@@ -67,133 +59,53 @@ namespace day3
 
         return paths;
     }
-    
-    void PrintGrid(std::deque<std::deque<char>> const& grid)
+
+    std::vector<std::pair<int, int>> TracePath(Path const& path)
     {
-        for (auto const& row : grid)
+        std::vector<std::pair<int, int>> tracedPath;
+
+        int x = 0;
+        int y = 0;
+        for (auto const& step : path.steps)
         {
-            for (auto const& col : row)
+            for (int i = 0; i < step.amount; ++i)
             {
-                std::cout << col << " ";
+                if (step.direction == UP)
+                    y++;
+                else if (step.direction == DOWN)
+                    y--;
+                else if (step.direction == RIGHT)
+                    x++;
+                else if (step.direction == LEFT)
+                    x--;
+    
+                // For some reason vscode seems to fail here for big input files - has errors when inserting > 498 pairs in the vector...
+                tracedPath.push_back({x, y});
             }
-            std::cout << std::endl;
         }
+
+        return tracedPath;
     }
 
     int Calculate()
     {
         std::vector<Path> paths = GetInput();
+        std::vector<std::pair<int, int>> tracedPathOne = TracePath(paths[0]);
+        std::vector<std::pair<int, int>> tracedPathTwo = TracePath(paths[1]);
 
-        std::deque<std::deque<char>> grid = { { '0' } }; // rows by columns (Y by X)
-        int centralX = 0;   // X is the column number
-        int centralY = 0;   // Y is the row number
+        std::sort(tracedPathOne.begin(), tracedPathOne.end());
+        std::sort(tracedPathTwo.begin(), tracedPathTwo.end());
 
-        PrintGrid(grid);
+        std::vector<std::pair<int, int>> intersections;
+        std::set_intersection(tracedPathOne.begin(), tracedPathOne.end(), tracedPathTwo.begin(), tracedPathTwo.end(), std::back_inserter(intersections));
 
-        std::vector<Intersection> intersections;
-
-        int pathIndex = 1;
-        for (auto const& path : paths)
+        std::vector<int> distances(intersections.size());
+        for (int i = 0; i < intersections.size(); ++i)
         {
-            int pathCurrentX = centralX;
-            int pathCurrentY = centralY;
-
-            int stepIndex = 1;
-            for (auto const& step : path.steps)
-            {
-                for (int i = 1; i <= step.amount; ++i)
-                {
-                    if (step.direction == UP && --pathCurrentY < 0)
-                    {
-                        grid.emplace_front(std::deque<char>(grid[0].size(), '-'));
-
-                        pathCurrentY = 0;   // New grid row will be inserted so keep our position at 0
-                        
-                        // As we've inserted a new grid row we need to update the central position & all intersection positions
-                        centralY++;
-                        for (auto& intersection : intersections)
-                        {
-                            intersection.y++;
-                        }
-                    }
-                    else if (step.direction == DOWN && ++pathCurrentY >= grid.size())
-                    {
-                        grid.emplace_back(std::deque<char>(grid[0].size(), '-'));
-                    }
-                    else if (step.direction == RIGHT && ++pathCurrentX >= grid[pathCurrentY].size())
-                    {
-                        int const newSize = grid[0].size() + (step.amount - i + 1);
-                        for (auto& row : grid)
-                        {
-                            row.resize(newSize, '-');
-                        }
-                    }
-                    else if (step.direction == LEFT && --pathCurrentX < 0)
-                    {
-                        for (auto& row : grid)
-                        {
-                            row.emplace_front('-');
-                        }
-
-                        pathCurrentX = 0;   // New grid column will be inserted so keep our position at 0
-                        
-                        // As we've inserted a new grid row we need to update the central position & all intersection positions
-                        centralX++;
-                        for (auto& intersection : intersections)
-                        {
-                            intersection.x++;
-                        }
-                    }
-
-                    if (grid[pathCurrentY][pathCurrentX] == '-')
-                    {
-                        grid[pathCurrentY][pathCurrentX] = path.id;
-                    }
-                    else if (grid[pathCurrentY][pathCurrentX] != path.id)
-                    {
-                        grid[pathCurrentY][pathCurrentX] = '+';
-
-                        Intersection newIntersection;
-                        newIntersection.x = pathCurrentX;
-                        newIntersection.y = pathCurrentY;
-                        newIntersection.distanceFromCentralPort = std::abs(newIntersection.x - centralX) + std::abs(newIntersection.y - centralY);
-                        
-                        intersections.push_back(newIntersection);
-                    }
-
-                    system("cls");
-                    std::cout << "Path " << pathIndex << "/" << paths.size() << " - Step " << stepIndex << "/" << path.steps.size() << std::endl;
-                    std::cout << step.direction << step.amount << " (" << i << ")" << std::endl;
-                    // PrintGrid(grid);
-                    // std::cout << std::endl;
-                    // std::cout << "C: " << centralX << ", " << centralY << std::endl;
-                    // for (auto const& intersection : intersections)
-                    // {
-                    //     std::cout << "+: " << intersection.x << ", " << intersection.y << " <-> " << intersection.distanceFromCentralPort << std::endl;
-                    // }
-                    // std::this_thread::sleep_for(UI_DELAY);
-                }
-
-                stepIndex++;
-            }
-
-            pathIndex++;
+            distances[i] = std::abs(intersections[i].first) + std::abs(intersections[i].second);
         }
 
-        system("cls");
-        PrintGrid(grid);
-        std::cout << std::endl;
-        std::cout << "C: " << centralX << ", " << centralY << std::endl;
-        
-        std::sort(std::begin(intersections), std::end(intersections), [](auto const& lhs, auto const& rhs)
-        {
-            return lhs.distanceFromCentralPort < rhs.distanceFromCentralPort;
-        });
-        for (auto const& intersection : intersections)
-        {
-            std::cout << "+: " << intersection.x << ", " << intersection.y << " <-> " << intersection.distanceFromCentralPort << std::endl;
-        }
-
-        return intersections[0].distanceFromCentralPort;
+        std::sort(distances.begin(), distances.end());
+        return distances[0];
     }
 } // namespace day3
